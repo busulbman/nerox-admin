bbb3959976a6cac3a4f5f5aeb0133a72a5e1c5ab
NurAli Khutiev <akchemapp@gmail.com>
2026-06-21 15:44:35 +0300

    Update metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
